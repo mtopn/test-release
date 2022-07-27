d623b37868b17bf91bd2d6456d391a92c40b0d77
mtopn <mengting@opn.ooo>
2022-07-27 17:20:04 +0700

    feat: add new route '/'

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.text(new Date());
+});
+
 app.listen(PORT, (err) => {
   if (err) console.log(err);
   console.log(`server is running on port ${PORT}`);
