9bef3e8078f9966ce79ee12b95f3aa11b1113ea7
mtopn <mengting@opn.ooo>
2022-07-29 09:16:57 +0700

    feat: allow get time now

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
+  res.text('GET');
+});
+
+app.get('/time', (req, res) => {
   res.text(new Date());
 });
 
 app.post('/', (req, res) => {
   res.text('POST');
+});
+
+app.put('/', (req, res) => {
+  res.text('PUT');
 });
 
 app.put('/', (req, res) => {
